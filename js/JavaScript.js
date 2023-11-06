4f808979737fb3392161f1e7d585789cb44a1d4e
Julia Farias de Queiroz <133894616+juhfarias31@users.noreply.github.com>
2023-11-06 14:20:49 -0300

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,24 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function rand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  
+function aleatorio(){
+  var num = rand(1,8); alert(num);
+  alert(num);
+}
